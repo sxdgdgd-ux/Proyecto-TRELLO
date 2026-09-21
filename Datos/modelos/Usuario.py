9e1a874f8707dd2302bebfb4ab5a8465bbe51c91
sxdgdgd-u <sxdgdgd@gmail.com>
2026-09-21 18:11:00 -0300

    CLASS

---
+++ Datos/modelos/Usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, id_usuario, nombre, correo_electronico, contrasena):
+        self.__id_usuario = id_usuario
+        self.__nombre = nombre
+        self.__correo_eletronico = correo_eletronico
+        self.__contrasena = contrasena
+        
---
+++ Datos/modelos/proyecto.py
@@ -0,0 +1,7 @@
+class Proyecto:
+    def __init__(self, id_proyecto, nombre, descripcion, fecha_inicio, fecha_fin):
+        self.__id_proyecto = id_proyecto
+        self.__nombre = nombre
+        self.__descripcion = descripcion
+        self.__fecha_inicio = fecha_inicio
+        self.__fecha_fin = fecha_fin
